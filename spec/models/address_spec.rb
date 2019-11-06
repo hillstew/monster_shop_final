5c7046c34c552fe43679a683c94332132f3da26c
Hillary Stewart <hillarystewart4427@gmail.com>
2019-11-06 07:40:50 -0700

    Add spec for has_orders?

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -12,4 +12,27 @@ RSpec.describe Address do
     it { should validate_presence_of :state }
     it { should validate_presence_of :zip }
   end
+
+  describe 'instance methods' do
+    before :each do
+      @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @user = User.create!(name: 'Megan', email: 'megan@example.com', password: 'securepassword')
+      @address_1 = @user.addresses.create(nickname: "work", address: '123 Main St', city: 'Marianna', state: 'Colorado', zip: 34567)
+      @address_2 = @user.addresses.create(nickname: "work", address: '123 Main St', city: 'Marianna', state: 'Colorado', zip: 34567)
+      @order_1 = @user.orders.create!(status: "packaged", address_id: @address_1.id)
+      @order_2 = @user.orders.create!(status: "shipped", address_id: @address_1.id)
+      @order_item_1 = @order_1.order_items.create!(item: @ogre, price: @ogre.price, quantity: 5, fulfilled: true)
+      @order_item_2 = @order_2.order_items.create!(item: @hippo, price: @hippo.price, quantity: 2, fulfilled: true)
+      @order_item_3 = @order_2.order_items.create!(item: @ogre, price: @ogre.price, quantity: 2, fulfilled: false)
+    end
+
+    it ".shipped_orders?" do
+      expect(@address_1.has_orders?).to eq(true)
+      expect(@address_2.has_orders?).to eq(false)
+    end
+  end
 end
